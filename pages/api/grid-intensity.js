1d52938dda7b1f644e445f421237035b27a94bf3
Samuel Pitoňák <sampittko@gmail.com>
2022-10-22 14:47:37 +0200

    refactor: returns co2 rating

--- pages/api/grid-intensity.js
+++ pages/api/grid-intensity.js
@@ -39,7 +39,7 @@ async function handler(req, res) {
 
   if (gridIntensityResponse.status === 200) {
     const value = await gridIntensityResponse.json();
-    res.status(200).json({ value });
+    res.status(200).json({ value: value[0].rating });
   } else {
     res.status(200).json({ value: null });
   }
