6af84c7c77ee688bef8cf42b790f7a7bd1923e22
Samuel Pitoňák <sampittko@gmail.com>
2022-10-22 14:35:23 +0200

    refactor: grid-intensity.js

--- pages/api/grid-intensity.js
+++ pages/api/grid-intensity.js
@@ -5,6 +5,7 @@ function distance({ lat: lat1, lon: lon1 }, { lat: lat2, lon: lon2 }) {
   const φ1 = (lat1 * Math.PI) / 180;
   const φ2 = (lat2 * Math.PI) / 180;
   const Δλ = ((lon2 - lon1) * Math.PI) / 180;
+
   return Math.acos(Math.sin(φ1) * Math.sin(φ2) + Math.cos(φ1) * Math.cos(φ2) * Math.cos(Δλ)) * R;
 }
 
@@ -29,10 +30,13 @@ async function handler(req, res) {
   const {
     query: { lat, lon },
   } = req;
+
   const closestAzureRegion = getClosestAzureRegion({ lat, lon }, AZURE_REGIONS);
+
   const gridIntensityResponse = await fetch(
     `https://carbon-aware-api.azurewebsites.net/emissions/bylocation?location=${closestAzureRegion}`,
   );
+
   if (gridIntensityResponse.status === 200) {
     const value = await gridIntensityResponse.json();
     res.status(200).json({ value });
