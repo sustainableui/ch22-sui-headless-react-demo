125e1deb5c275f29eec6c647f49acd113c2bf0bc
Samuel Pitoňák <sampittko@gmail.com>
2022-10-31 17:16:44 +0100

    chore: updates webpack loaders for lib

--- next.config.js
+++ next.config.js
@@ -2,6 +2,14 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  webpack: (config, options) => {
+    config.module.rules.push({
+      test: /sui-headless-react.*\.(ts|js)x?$/,
+      use: options.defaultLoaders.babel,
+    });
+
+    return config;
+  },
 };
 
 module.exports = nextConfig;
